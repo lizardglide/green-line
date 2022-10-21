d418cd6f9b4b20d60699e462cbb15a765749c082
Boumaaz <fatima-ezzahra.boumaaz@capgemini.com>
2022-10-21 16:46:23 +0200

    add slider
    Signed-off-by: Boumaaz <fatima-ezzahra.boumaaz@capgemini.com>

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    private float score = 0;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        this.score +=  Time.deltaTime;
+    }
+
+    public float Score {
+        get {
+            return this.score;
+        }
+    }
+}
